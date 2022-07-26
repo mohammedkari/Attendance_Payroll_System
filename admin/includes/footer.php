ff330daf6b6026c52340bac3f25a09f6cf3c46ba
mohammedkari <101977018+mohammedkari@users.noreply.github.com>
2022-07-26 20:48:57 +0000

    first work

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Brought To You By Mohammed Abdul Karim</b>
+    </div>
+    <strong>Copyright &copy; 2022 </strong>
+</footer>
